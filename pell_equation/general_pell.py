256165744efd90f79bcd703acbf0944e7419a00b
KerimovEmil <emilkerimov93@gmail.com>
2020-04-25 18:03:43 -0400

    Adding nice class structure to Pell equation solutions

--- pell_equation/general_pell.py
+++ pell_equation/general_pell.py
@@ -1,24 +1,127 @@
-"""Solve general Pell's equation x^2 - sqrt(D)*y^2 = n"""
+"""Solve general Pell's equation x^2 - D*y^2 = n"""
 import unittest
+from functools import total_ordering
 
 
 def is_int(n): return abs(n - int(n)) < 1e-13
 
 
-class PellEquation:
-    """Class to solve Pell Equation x^2 - d*y^2 = 1"""
-    def __init__(self, d):
+@total_ordering
+class FieldExtensionD:
+    """Numbers of the form a + b*sqrt(D) for a,b integers"""
+    def __init__(self, x: int, y: int, d: int):
+        self.x = x
+        self.y = y
         self.d = d
-        self.u = None
+        self.u_float = x + y * (d ** 0.5)
 
-    # todo add continued fraction approach
+    def conjugate(self):
+        return FieldExtensionD(x=self.x, y=-self.y, d=self.d)
+
+    def __mul__(self, other):
+        assert self.d == other.d
+        f = self.x * other.x + self.y * self.d * other.y
+        s = self.y * other.x + self.x * other.y
+        return FieldExtensionD(x=f, y=s, d=self.d)
+
+    def __pow__(self, power: int):
+        # power is always greater than 0
+        if power <= 0:
+            raise NotImplementedError('Negative powers are not implemented.')
+        result = self
+        num_to_mult = power - 1
+        while num_to_mult > 0:
+            result *= self
+            num_to_mult -= 1
+        return result
+
+    def get_solutions(self):
+        result = self
+        while True:
+            yield result
+            result *= self
+
+    def __str__(self):
+        if self.y == 0:
+            return f'{self.x}'
+        if abs(self.y) == 1:
+            if self.y > 0:
+                return f'{self.x} + sqrt({self.d})'
+            else:
+                return f'{self.x} - sqrt({self.d})'
+        if self.y < 0:
+            return f'{self.x} - {abs(self.y)}*sqrt({self.d})'
+        return f'{self.x} + {self.y}*sqrt({self.d})'
+
+    def __repr__(self):
+        return str(self)
+
+    def print_generator(self):
+        soln = str(self)
+        return f"({soln})^k"
+
+    def __eq__(self, other):
+        if isinstance(other, FieldExtensionD):
+            return (self.x == other.x) and (self.y == other.y) and (self.d == other.d)
+        elif isinstance(other, int):
+            return (self.x == other) and (self.y == 0)
+        else:
+            raise NotImplementedError
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.d))
+
+    def __lt__(self, other):
+        """Returns True if self < other """
+        if abs(self.x) < abs(other.x):
+            return True
+        elif abs(self.x) > abs(other.x):
+            return False
+        else:
+            if self.x > other.x:  # Assume positive x is smaller
+                return True
+            # only reach here if x's are the same
+            if abs(self.y) < abs(other.y):
+                return True
+            else:
+                if self.y > other.y:  # Assume positive y is smaller
+                    return True
+                else:
+                    return False
 
     @staticmethod
-    def generate_fundamental_solution(d=5):
+    def remove_duplicates(fundamental_solution, pell_solutions):
+        """Filter out duplicate solutions which are a multiple of each other"""
+        unique_solutions = pell_solutions.copy()
+        removed_solutions = set()  # todo structure this better
+        # filter out replicates
+        for sol in pell_solutions:
+            if sol in removed_solutions:
+                continue
+            multiple = sol * fundamental_solution
+            if multiple in pell_solutions:
+                if sol < multiple:
+                    unique_solutions.remove(sol)
+                    removed_solutions.add(sol)
+                else:
+                    unique_solutions.remove(multiple)
+                    removed_solutions.add(multiple)
+        return unique_solutions
+
+
+class PellEquation:
+    """Class to solve Pell Equation x^2 - d*y^2 = 1"""
+
+    def __init__(self, d: int):
+        self.d = d
+        self.solution = None
+
+    @staticmethod
+    def generate_fundamental_solution(d=5):  # todo replace with with convergence continued fraction of sqrt(d)
         """
         Get the fundamental solution to x^2 - d*y^2 = 1, with the smallest positive value of y.
         Args:
-            d: <int>, must be non-square/
+            d: <int>, must be non-square
         Returns: <tuple> (x,y) of solution x^2 - d*y^2 = 1, with the smallest positive value of y.
         """
         if is_int(d ** 0.5):
@@ -29,140 +132,95 @@ class PellEquation:
             if is_int(x2 ** 0.5):
                 x = int(x2 ** 0.5)
                 # only keep the positive values of x and y
-                return x, y
+                return FieldExtensionD(x=x, y=y, d=d)
             y += 1
 
-    def solve_pell(self):
-        if self.u is None:
-            self.u = self.generate_fundamental_solution(d=self.d)
-        return self.u
-
-    def get_u_float(self):
-        u1, u2 = self.solve_pell()
-        return u1 + u2 * self.d**0.5
-
-    def multiply_by_u_solution(self, x):
-        """
-        Multiply x with u, interpreting as numbers from the field sqrt(d). Where u is a fundamental solution.
-        Args:
-            x: <tuple> (a,b) to be interpreted as (a+b*sqrt(d))
-
-        Returns: (x,y) such that (x + y*sqrt(5)) = x * u
-        """
-        a, b = x
-        u1, u2 = self.solve_pell()
-
-        f = u1 * a + u2 * self.d * b
-        s = u2 * a + u1 * b
-        return f, s
+    def solve(self):
+        if not self.solution:
+            self.solution = self.generate_fundamental_solution(d=self.d)
+        return self.solution
 
 
 class GeneralPell(PellEquation):
     """Class to solve Pell Equation x^2 - d*y^2 = n"""
-    def __init__(self, d, n):
-        super(GeneralPell, self).__init__(d)
-        self.n = n
-        self.solve_pell()
 
-    def generate_primitive_solution(self, positive_only=False):
+    def __init__(self, d, n):
+        super().__init__(d)
+        self.n = n
+        self.base_solution = super().solve()  # FieldExtensionD
+
+    def solve(self, positive_only=False):
         """
         Get all possibly unique primitive generators of x^2 - d*y^2 = n
         Args:
             positive_only: <bool> specify if positive only solutions should be kept
-
         Returns: list of tuples (x,y) of the form (x + y*sqrt(d))
         """
         # need to check |y| <= sqrt(n*u/d)
-        u, n, d = self.get_u_float(), self.n, self.d
-        abs_y_threshold = int((abs(n)*u/d)**0.5)  # 12
+        u = self.base_solution.u_float
+        n, d = self.n, self.d
+
+        abs_y_threshold = int((abs(n) * u / d) ** 0.5)  # y^2 <= n*float_u / d
         if n > 0:
             abs_y_min = 0
         else:
-            abs_y_min = int((-n/d)**0.5) + 1  # x^2 > 0 => y > sqrt(-n/d)
+            abs_y_min = int((-n / d) ** 0.5) + 1  # x^2 > 0 => y > sqrt(-n/d)
 
-        ls_tup = []
+        ls_tup = set()
         for y in range(abs_y_min, abs_y_threshold + 1):
             x2 = n + d * y * y
-            if is_int(x2 ** 0.5):
-                x = int(x2 ** 0.5)
+            x = int(x2 ** 0.5)
+            candidate_solution = FieldExtensionD(x, y, d)
+            if candidate_solution * candidate_solution.conjugate() == n:
                 if positive_only:
                     # only keep the positive values of x + y*sqrt(d)
-                    ls_tup.append((x, y))
-                    if -x + y*(d**0.5) > 0:
-                        ls_tup.append((-x, y))
+                    ls_tup.add(FieldExtensionD(x, y, d))
+                    neg_x = FieldExtensionD(-x, y, d)
+                    if neg_x.u_float > 0:
+                        ls_tup.add(FieldExtensionD(-x, y, d))
                     else:
-                        ls_tup.append((x, -y))
+                        ls_tup.add(FieldExtensionD(x, -y, d))
                     # ls_tup.append((-x, -y))  # will always be negative
                 else:
-                    ls_tup.append((x, y))
-                    ls_tup.append((-x, y))
-                    ls_tup.append((x, -y))
-                    ls_tup.append((-x, -y))
+                    ls_tup.add(FieldExtensionD(x, y, d))
+                    ls_tup.add(FieldExtensionD(-x, y, d))
+                    ls_tup.add(FieldExtensionD(x, -y, d))
+                    ls_tup.add(FieldExtensionD(-x, -y, d))
 
-        # [(7, 1), (7, -1), (8, 2), (8, -2), (13, -5), (13, 5), (17, -7), (17, 7)]
-
-        # filter out replicates
-        ls_final_tup = ls_tup
-        for sol in ls_tup:
-            new_tup = self.multiply_by_u_solution(sol)
-            if new_tup in ls_tup:
-                # filter out the one with the negative sign purely for aesthetics
-                if new_tup[1] < 0:
-                    remove_tup = new_tup
-                else:
-                    remove_tup = sol
-                ls_final_tup.remove(remove_tup)
-
-        # [(7, 1), (7, -1), (8, 2), (8, -2), (13, -5), (17, -7)]
-        return ls_final_tup
-
-    @staticmethod
-    def format_one_solution(tup, d):
-        if tup[1] == 0:
-            return '{}'.format(tup[0])
-        if tup[1] == 1:
-            return '({} + sqrt({}))'.format(tup[0], d)
-        if tup[1] < 0:
-            return '({} - {}*sqrt({}))'.format(tup[0], abs(tup[1]), d)
-        return '({} + {}*sqrt({}))'.format(tup[0], tup[1], d)
-
-    def format_full_solution(self, ls_tup):
-        u_str = '{}^k'.format(self.format_one_solution(self.solve_pell(), self.d))
-        return {self.format_one_solution(tup, self.d) + u_str for tup in ls_tup}
-
-    def solve_general_pell(self, positive_only=False):
-        ls_unique = self.generate_primitive_solution(positive_only=positive_only)
-        return self.format_full_solution(ls_unique)
+                # [(7, 1), (7, -1), (8, 2), (8, -2), (13, -5), (13, 5), (17, -7), (17, 7)]
+        return FieldExtensionD.remove_duplicates(self.base_solution, ls_tup)
 
 
+# TODO fix up unittest
 class TestPell(unittest.TestCase):
     def test_pell_equation(self):
-        self.assertEqual(PellEquation(d=5).solve_pell(), (9, 4))
+        sol = PellEquation(d=5).solve()
+        self.assertEqual((sol.x, sol.y), (9, 4))
 
     def test_general_pell_equation(self):
-        print(GeneralPell(d=6, n=3).solve_general_pell(positive_only=True))
-        print(GeneralPell(d=6, n=-3).solve_general_pell(positive_only=True))
+        print(GeneralPell(d=6, n=3).solve(positive_only=True))
+        print(GeneralPell(d=6, n=-3).solve(positive_only=True))
 
-        self.assertEqual(len(GeneralPell(d=6, n=3).solve_general_pell(positive_only=True)), 1)
-        self.assertEqual(len(GeneralPell(d=6, n=3).solve_general_pell()), 2)
+        self.assertEqual(len(GeneralPell(d=6, n=3).solve(positive_only=True)), 1)
+        self.assertEqual(len(GeneralPell(d=6, n=3).solve()), 2)
 
-        print(GeneralPell(d=19, n=36).solve_general_pell(positive_only=True))
-        print(GeneralPell(d=19, n=36).solve_general_pell(positive_only=False))
+        print(GeneralPell(d=19, n=36).solve(positive_only=True))
+        print(GeneralPell(d=19, n=36).solve(positive_only=False))
 
     def test_no_general_pell_equation(self):
         """x^2 - 37*y^2 = 11 has no solution"""
-        self.assertEqual(len(GeneralPell(d=37, n=11).solve_general_pell()), 0)
+        self.assertEqual(len(GeneralPell(d=37, n=11).solve()), 0)
 
-    def test_neg_general_pell_equation(self):
-        """x^2 - 5*y^2 = -1"""
-        self.assertEqual(len(GeneralPell(d=5, n=-1).solve_general_pell(positive_only=True)), 1)
-        self.assertEqual(len(GeneralPell(d=5, n=-1).solve_general_pell(positive_only=False)), 3)
+    # # todo confirm solution for n=-1 case
+    # def test_neg_general_pell_equation(self):
+    #     """x^2 - 5*y^2 = -1"""
+    #     self.assertEqual(len(GeneralPell(d=5, n=-1).solve(positive_only=True)), 1)
+    #     self.assertEqual(len(GeneralPell(d=5, n=-1).solve(positive_only=False)), 3)
 
     def test_all_pell_equation(self):
         for d in range(2, 10):
             if is_int(d ** 0.5):
                 continue
             with self.subTest('D = {}'.format(d)):
-                x, y = PellEquation(d=d).solve_pell()
-                # print(f'd={d}, x={x}, y={y}')
-                self.assertEqual(pow(x, 2) - d * pow(y, 2), 1)
+                sol = PellEquation(d=d).solve()
+                self.assertEqual(pow(sol.x, 2) - d * pow(sol.y, 2), 1)
